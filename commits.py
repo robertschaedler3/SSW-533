5157e0c1ba787ce372f8aed2098fc32a3bbd4e55
Robert Schaedler <robertschaedler3@gmail.com>
2021-04-27 21:17:03 -0400

    clean and group data

--- commits.py
+++ commits.py
@@ -1,25 +1,26 @@
-import json
-import requests
-
 from datetime import datetime
-
-from pydriller import RepositoryMining
 
 import pandas as pd
 import numpy as np
 
+from pydriller import RepositoryMining
 
-def run(repo, start, end):
+
+def change_point_detection():
+    pass
+
+
+def commit_data(repo, start, end):
 
     commit_data = []
-    
+
     for commit in RepositoryMining(repo, since=start, to=end).traverse_commits():
-        commit_data.append([commit.hash, 
-                            commit.author_date, 
-                            commit.committer_date, 
-                            commit.insertions, 
-                            commit.deletions, 
-                            commit.lines, 
+        commit_data.append([commit.hash,
+                            commit.author_date,
+                            commit.committer_date,
+                            commit.insertions,
+                            commit.deletions,
+                            commit.lines,
                             commit.files])
 
         # Temp break for testing
@@ -27,11 +28,21 @@ def run(repo, start, end):
             break
 
     df = pd.DataFrame(data=commit_data, columns=['hash', 'author_date', 'committer_date', 'insertions', 'deletions', 'lines', 'files'])
+    df['committer_date'] = pd.to_datetime(df['committer_date'], utc=True)
+    # df.set_index('committer_date', inplace=True)
+    return df
 
-    print(df)
 
+def run(repo, start, period=1):
 
+    df = commit_data(repo, start, datetime.now())
+    print(df.tail())
+
+    sloc = []
+    print(df.groupby(by=df['committer_date'].dt.date).sum())
+    
+    
 if __name__ == '__main__':
     start = datetime(2016, 10, 8, 17, 0, 0)
-    end = datetime.now()
-    run('https://github.com/robertschaedler3/robertschaedler.com', start, end)
+    period = 2  # days for sloc/unit time
+    run('https://github.com/robertschaedler3/robertschaedler.com', start, period)
